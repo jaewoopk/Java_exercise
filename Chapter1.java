e9005de7841cea447b8a7e8300b113b9cb9ca766
jaewoopk <jaewoopk000@naver.com>
2022-08-19 21:40:41 +0900

    Day 1 Practice Chapter1, 2

---
+++ Chapter1.java
@@ -0,0 +1,9 @@
+class Chapter1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("First Java Program");
+
+        System.out.println("2 + 5 = " + 2 + 5);
+        System.out.println("2 + 5 = " + (2 + 5));
+    }
+}
---
+++ Chapter2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Chapter2 {
+    public static void main(String[] args) {
+        char japan1 = 0x3041;
+        char japan2 = 0x3051;
+        char japan3 = 0x3061;
+        char japan4 = 0x3071;
+
+        System.out.println(japan1);
+        System.out.println(japan2);
+        System.out.println(japan3);
+        System.out.println(japan4);
+
+    }    
+}
