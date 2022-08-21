b224428e8b68ca2dc2e8f30aa799e6b404d75fcc
jaewoopk <jaewoopk000@naver.com>
2022-08-21 12:03:41 +0900

    Day 2 Practice Chapter3

---
+++ Chapter3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Chapter3 {
+	public static void main(String[] args) {
+		int n;
+		short s_n;
+
+		n = 7;
+		System.out.println("n = " + 7);
+		System.out.println("n2 = " + n);
+		s_n = (short)(7 + 8);
+		System.out.println("s_n = " + 15);
+		System.out.println("s_n2 = " + s_n);
+	}
+}
