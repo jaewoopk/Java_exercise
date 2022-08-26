66d77ee3705c41b41240f83473463987339c452f
jaewoopk <jaewoopk000@naver.com>
2022-08-26 15:36:04 +0900

    Day 5 Practice Chapter 8

---
+++ com/wxfx/smart/CircleUsing.java
@@ -0,0 +1,8 @@
+package com.wxfx.smart;
+
+public class CircleUsing {
+    public static void main(String[] args) {
+        Circle a = new Circle(3.5);
+        System.out.println(a.getArea());
+    }
+}
