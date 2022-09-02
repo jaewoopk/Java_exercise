84a1ea760fe8d55efecdf8da732877df80723053
jaewoopk <jaewoopk000@naver.com>
2022-09-02 11:52:33 +0900

    Day 6 Practice Chapter 10

---
+++ Chapter10.java
@@ -0,0 +1,25 @@
+import static java.lang.Math.*;
+
+class Accumulator {
+    static int sum = 0;
+
+    static void add(int i) {
+        sum += i;
+    }
+    
+    static void showResult() {
+        System.out.println(sum);
+    }
+}
+
+public class Chapter10 {
+    public static void main(String[] args) {
+        System.out.println(abs(1));
+        System.out.println(min(10, -5));
+        System.out.println(PI);
+        for (int i = 0 ; i < 10; i++) {
+            Accumulator.add(i);
+        }
+        Accumulator.showResult();
+    }
+}
