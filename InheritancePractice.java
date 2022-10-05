f858bca09bfd729a6ae9ebdb124849da8895e01c
jaewoopk <jaewoopk000@naver.com>
2022-10-05 16:19:27 +0900

    Day 14 Whole known about constructor, inheritance, public, private, protected, static etc

---
+++ InheritancePractice.java
@@ -0,0 +1,77 @@
+class MyString {
+    protected String str;
+    MyString() {
+        str = "N/A";
+    };
+    MyString(String in) {
+        str = in;
+    }
+    void print() {
+        System.out.println("[STR] = " + str);
+    }
+}
+
+
+class MyColorString extends MyString{
+    String color;
+    MyColorString() {
+        super("hello");
+        color = "black";
+    }
+    MyColorString(String in, String c) {
+        super(in);
+        color = c;
+    }
+    void colorPrint() {
+        System.out.println("[ClrSTR] " + str + ", " + "color = " + color);
+    };
+}
+
+class MySimplestClass {
+
+}
+
+public class InheritancePractice {
+    static void function(Object in) {
+
+    }
+    public static void main(String[] args) {
+        // StaticTest a = new StaticTest();
+        // a.func1();
+        // StaticTest.func1();
+        int a;
+        a = 20;
+        System.out.println(a);
+        MyColorString abc = new MyColorString("Sejong", "Magenta");
+        //abc.str = "Sejong univ.";
+        //abc.color = "yellow";
+        abc.colorPrint();
+        abc.print();
+
+        MyString b = abc;
+        System.out.println(b.str.length());
+
+        MySimplestClass simple = new MySimplestClass();
+        System.out.println(simple.hashCode());
+
+        MyString c = abc;
+
+        MyColorString d = (MyColorString)c; // 부모인척하는 자식일 때
+        
+    }
+}
+
+
+
+// class StaticTest {
+//     static int value;
+//     private StaticTest(){};
+//     static void func1() {
+//         int a;
+//         a = 10;
+//         System.out.println(a);
+//     };
+//     static void func2() {};
+//     static void func3() {};
+//     static void func4() {};
+// }
