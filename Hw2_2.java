e80618ea7b1015d03c0bc5e6d01b6cb29645322c
jaewoopk <jaewoopk000@naver.com>
2022-10-22 13:57:09 +0900

    Day 20 Complete about Hw2_2 with abstract class

--- Hw2_2.java
+++ Hw2_2.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 abstract class PairMap {
     protected String keyArray [];
     protected String valueArray [];
@@ -12,32 +9,69 @@ abstract class PairMap {
 }
 
 class Dictionary extends PairMap {
-    Map<String, String> hm;
+    public int index = 0;
     Dictionary() {
-        hm = new HashMap<String, String>();
+        keyArray = null;
+        valueArray = null;
+    }
+
+    Dictionary(int n) {
+        keyArray = new String[n];
+        valueArray = new String[n];
     }
     @Override
     String get(String key) {
-        // TODO Auto-generated method stub
-        return key;
+        int findIndex = 0;
+
+        for (String k : keyArray) {
+            if (k == key) {
+                break ;
+            }
+            findIndex++;
+        }
+        if (findIndex >= 10) {
+            return null;
+        }
+        return valueArray[findIndex];
     }
 
     @Override
     void put(String key, String value) {
-        // TODO Auto-generated method stub
-        
+        for (int i = 0; i < index; i++) {
+            if (keyArray[i] == key) {
+                valueArray[i] = value;
+                return ;
+            }
+        }
+        keyArray[index] = key;
+        valueArray[index++] = value;
     }
 
     @Override
     String delete(String key) {
-        // TODO Auto-generated method stub
-        return null;
+        int findIndex = 0;
+        String tmp = null;
+
+        for (int i = 0; i < index; i++) {
+            if (keyArray[i] == key) {
+                findIndex = i;
+                break ;
+            }
+        }
+        tmp = valueArray[findIndex];
+        for (int i = keyArray.length - 2; i >= findIndex; i--) {
+            keyArray[i] = keyArray[i + 1];
+            valueArray[i] = valueArray[i + 1];
+        }
+        keyArray[keyArray.length - 1] = null;
+        valueArray[valueArray.length - 1] = null;
+        index--;
+        return tmp;
     }
 
     @Override
     int length() {
-        // TODO Auto-generated method stub
-        return 0;
+        return index;
     }
 
 }
@@ -45,6 +79,14 @@ class Dictionary extends PairMap {
 
 public class Hw2_2 {
     public static void main(String[] args) {
+        Dictionary dic = new Dictionary(10);
 
+        dic.put("황기태", "자바");
+        dic.put("이재문", "파이선");
+        dic.put("이재문", "C++");
+        System.out.println("이재문의 값은 " + dic.get("이재문"));
+        System.out.println("황기태의 값은 " + dic.get("황기태"));
+        dic.delete("황기태");
+        System.out.println("황기태의 값은 " + dic.get("황기태"));
     }
 }
