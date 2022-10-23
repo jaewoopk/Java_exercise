bf20416f83355180d045719df87569e07f052345
jaewoopk <jaewoopk000@naver.com>
2022-10-23 16:58:27 +0900

    Day 21 Studying about java but It's hard for memorize all of it

--- ForExam.java
+++ ForExam.java
@@ -1,24 +1,40 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
-class Employee implements Comparable<Employee>{
-    String name;
-    Employee(String in) {name = in;}
-    @Override
-    public int compareTo(Employee o) {
-        // TODO Auto-generated method stub
-        if (this.name.compareTo(o.name) > 0) {
-            return 1;
-        }
-        else if (this.name.compareTo(o.name) < 0)
-            return -1;
-        return 0;
-    }
+// class Employee implements Comparable<Employee>{
+//     String name;
+//     Employee(String in) {name = in;}
+//     @Override
+//     public int compareTo(Employee o) {
+//         // TODO Auto-generated method stub
+//         if (this.name.compareTo(o.name) > 0) {
+//             return 1;
+//         }
+//         else if (this.name.compareTo(o.name) < 0)
+//             return -1;
+//         return 0;
+//     }
 
     
-}
+// }
 
 public class ForExam {
     public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<String>();
+
+        arr.add("a");
+        arr.add("b");
+        arr.add("c");
+
+        for (Iterator<String> str = arr.iterator(); str.hasNext();) {
+            if (str.next().equals("a")) {
+                str.remove();
+            }
+        }
+        for (String str : arr) {
+            System.out.println(str);
+        }
         // int a = 3;
         // int b = 4;
         // int c = 5;
@@ -31,16 +47,16 @@ public class ForExam {
         // System.out.println("s1 == s2"+(s1==s2));
         // System.out.println("s1 == s3"+(s1==s3));
 
-        Employee [] arr = new Employee[3];
+        // Employee [] arr = new Employee[3];
 
-        arr[0] = new Employee("Kim");
-        arr[1] = new Employee("Park");
-        arr[2] = new Employee("Lee");
+        // arr[0] = new Employee("Kim");
+        // arr[1] = new Employee("Park");
+        // arr[2] = new Employee("Lee");
 
-        Arrays.sort(arr);
-        for (Employee e : arr) {
-            System.out.println(e.name);
-        }
+        // Arrays.sort(arr);
+        // for (Employee e : arr) {
+        //     System.out.println(e.name);
+        // }
 
     }
 }
