bfb58e7e3331fb5aa2c95831e0f50f451cc44f03
jaewoopk <jaewoopk000@naver.com>
2022-11-30 19:01:54 +0900

    Day 35 Trying to solve about assignment4, it's failed but learn about Frame and Panel to execute it

--- JavaHW4.java
+++ JavaHW4.java
@@ -1,86 +1,108 @@
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.awt.AlphaComposite;
-import java.awt.Color;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.Shape;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-class Hw4ViewPanel extends JPanel {
+class Hw4ViewerPanel extends JPanel {
     ArrayList<Shape> rect = new ArrayList<>();
-    Hw4ViewPanel() {
-        add(new JLabel("hello"));
-        add(new JTextField("16"));
-        rect.add(new Rectangle2D.Float(70,0,340,150));
-        rect.add(new Rectangle2D.Float(90,18,300,115));
-        setLayout(new FlowLayout());
+    Hw4ViewerPanel() {
+        setLayout(null);
+
+        rect.add(new Rectangle2D.Float(0,0,340,150));
     }
 
     @Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);	
 		
-		Graphics2D g2 = (Graphics2D) g;
-        Graphics2D g3 = (Graphics2D) g;
-		
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+		Graphics2D g1;
+        g1 = (Graphics2D) g;
+		g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		g2.setComposite(AlphaComposite.getInstance
-				      (AlphaComposite.SRC_OVER, 0.9f));
-		
-        g3.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		g3.setComposite(AlphaComposite.getInstance
-				      (AlphaComposite.SRC_OVER, 0.7f));
-		GradientPaint gp = new GradientPaint
-				            (0,0, Color.GRAY, 350,350, Color.DARK_GRAY);
-		
-        GradientPaint gp2 = new GradientPaint
-	    		            (0,0, Color.DARK_GRAY, 350,350, Color.BLUE);
-		g2.setPaint(gp);
-        g3.setPaint(gp2);
-        g2.fill(rect.get(0));
-        g3.fill(rect.get(1));
+		g1.setComposite(AlphaComposite.getInstance
+				       (AlphaComposite.SRC_OVER, 0.7f));
+		GradientPaint gp1 = new GradientPaint
+				            (0,0, Color.DARK_GRAY, 0,200, Color.GRAY);
+        
+        g1.setPaint(gp1);
+        g1.fill(rect.get(0));
 	}
 }
 
-class Hw4ButtonPanel extends JPanel {
+class Hw4TextPanel extends JPanel {
+    ArrayList<Shape> rect = new ArrayList<>();
+    JTextField text = new JTextField();
+    Color col = new Color(80,140,80);
+    Hw4TextPanel() {
+        setLayout(null);
+
+        rect.add(new Rectangle2D.Float(0,0,300,110));
+    }
+
+    @Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);	
+		
+		Graphics2D g1;
+        g1 = (Graphics2D) g;
+		g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g1.setComposite(AlphaComposite.getInstance
+				       (AlphaComposite.SRC_OVER, 0.7f));
+		GradientPaint gp1 = new GradientPaint
+				            (0,0, col, 0,200, Color.DARK_GRAY);
+        
+        g1.setPaint(gp1);
+        g1.fill(rect.get(0));
+	}
+}
+
+public class JavaHW4 extends JFrame{
     final static String[][] str = {{"7", "8", "9", "c"},
                                  {"4", "5", "6", "+"},
                                  {"3", "2", "1", "-"},
                                  {"0", " ", " ", "="}};
-    Hw4ButtonPanel() {
-        JButton[][] but = new JButton[4][4];
+    JButton[][] but = new JButton[4][4];
+    JPanel viewer = new Hw4ViewerPanel();
+    JPanel texter = new Hw4TextPanel();
+    JPanel outPanel = new JPanel();
+    Font font = new Font("맑은 고딕", Font.BOLD, 30);
+    JavaHW4() {
+        setTitle("Homework2");
+        setSize(500, 500);
+        Container c = getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.BLACK);
+        outPanel.setBounds(80, 150, 340, 350);
+        outPanel.setBackground(Color.white);
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 but[i][j] = new JButton(str[i][j]);
-                add(but[i][j]);
+                but[i][j].setBounds(80 + (85 * (j)), 150 + (80 * (i)), 85 , 80);
+                but[i][j].setBackground(Color.ORANGE);
+                but[i][j].setFont(font);
+                c.add(but[i][j]);
             }
         }
-        setLayout(new GridLayout(4,4,2,2));
-    }
-}
-
-
-public class JavaHW4 extends JFrame{
-    JavaHW4() {
-        setTitle("Homework2");
-        add(new Hw4ButtonPanel());
-        setSize(500, 500);
+        viewer.setBounds(80, 0, 340, 150);
+        texter.setBounds(100, 20, 300, 110);
+        c.add(outPanel);
+        c.add(texter);
+        c.add(viewer);
         setVisible(true);
         setResizable(true);
+        setPreferredSize(new Dimension(500, 500));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+    
     public static void main(String[] args) {
         new JavaHW4();
     }
+    
 }
---
+++ LoginForm.java
@@ -0,0 +1,125 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LoginForm extends JFrame implements ActionListener {
+
+ // Frame을 구현하는 JFrame을 상속받음
+
+ 
+ /*
+ - JFrame은 Container클래스의 객체를 만들어서 add()메소드로 element를 추가했다면,
+ - JPanel은 JFrame안에 Pane을 추가해서, 그 Pane 안에 element를 add()메소드로 추가하는 방식이다.
+ */
+ 
+ JPanel panel1, panel2;
+ JTextField id;
+ JPasswordField password;
+ JButton confirm, cancel;
+ 
+ public LoginForm(){
+
+
+ 
+
+       panel1=new JPanel();  // pane 1을 Frame에 추가.
+       panel1.setLayout(new GridLayout(2,2)); 
+
+//2행 2열로 pane의 화면을 분할.
+
+       id=new JTextField(10);
+       password=new JPasswordField(10);
+
+ 
+
+  panel1.add(new JLabel("아이디", JLabel.LEFT));
+  panel1.add(id);
+  id.setToolTipText("ID를 입력하세요");
+  
+  panel1.add(new JLabel("비밀번호",JLabel.LEFT));
+  panel1.add(password);
+  password.setToolTipText("비밀번호를 입력하세요");
+  
+
+
+ 
+
+  panel2=new JPanel(); // pane 2를 Frame에 추가.
+  confirm=new JButton("확인");
+  cancel=new JButton("취소");
+  
+  confirm.addActionListener(this);
+  cancel.addActionListener(this);
+  // this : 클래스 자체를 가리킴
+  panel2.add(confirm);
+  panel2.add(cancel); 
+
+ 
+
+
+
+  getContentPane().add(panel1, "Center");
+  getContentPane().add(panel2, "South");
+  /* JFrame을 상속받았으므로, getContentPane()앞에 JFrame 객체가 생략된 것이다.
+   ( ex. JFrame e;
+
+   e.getContentPane() )
+
+   */
+  
+  this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+  this.setSize(200,120);
+  this.setVisible(true);
+  // this : 클래스 자체를 가리킴
+  
+
+ } //end LoginForm()
+ 
+
+ @Override
+ public void actionPerformed(ActionEvent ae) {
+  Object o=ae.getSource();
+  /* 어떤 클래스로 받아야 할 지 모를때
+    : 최상위클래스인 java.lang.Object를 상위클래스 객체로 놓으면 편하다.
+   */
+  
+  String db_id="1";
+  String db_pwd="1";
+  
+  if(o==confirm){
+   /* 로컬변수 db_id와 db_pwd가 문자값 1로 코딩내부적으로 정의되었고,
+
+id와 password에 1을 입력해야지만 참이 나온다. */
+
+   if(db_id.equals( id.getText().trim() ) && db_pwd.equals(password.getText().trim()) ){
+
+// trim() : 문자열에 존재하는 space를 제거.
+
+    
+    id.setText("Success");
+    password.setText("Success");
+
+   }else{
+
+    id.setText("Fail");
+    password.setText("Fail");
+    id.requestFocus();
+
+   } //end if(db_id, db_pwd)
+
+  }else if(o==cancel){
+
+   id.setText("");
+   password.setText("");
+   id.requestFocus();
+
+  } //end if( confirm else if cancel)
+ } //end actionPerformed
+ 
+ public static void main(String[] args) {
+
+  new LoginForm();
+
+ } //end main
+
+} 
